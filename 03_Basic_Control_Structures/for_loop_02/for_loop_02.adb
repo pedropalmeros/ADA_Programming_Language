7670e790933bf6cbddaa1e3534905066099bec6a
pedropalmeros <pedrofpalmeros@gmail.com>
2022-10-13 15:51:36 -0500

    [Add] for_loop_02 this is a reversible for from b to a step -1

---
+++ 03_Basic_Control_Structures/for_loop_02/for_loop_02.adb
@@ -0,0 +1,12 @@
+with Ada.Text_IO;
+
+procedure for_loop is
+begin
+    Ada.Text_IO.Put("|"); 
+
+    for iter in reverse 1 .. 400 loop
+        Ada.Text_IO.Put(Integer'Image(iter) & "|");
+    end loop;
+
+    Ada.Text_IO.New_Line;
+end for_loop;
